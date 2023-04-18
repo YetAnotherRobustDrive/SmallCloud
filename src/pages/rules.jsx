b444271433708f6004db41c9f8d64aab18fd8d8e
JaeikBae <0308bae@gmail.com>
2023-04-18 16:42:08 +0900

    arrange directoies

--- src/pages/rules.jsx
+++ src/pages/rules.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../header/header";
 import SidebarAdmin from "../sidebar/sidebarAdmin";
+import '../../css/admin.css'
 
-export default function Mypage() {
+export default function AdminRules() {
     return (
         <>
             <Header />
             <SidebarAdmin/>
+            <div className="bodyframe">
+                <div>test</div>
+            </div>
         </>
     )
 }
