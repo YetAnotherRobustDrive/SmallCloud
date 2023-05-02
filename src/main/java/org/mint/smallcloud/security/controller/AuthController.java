7bbe0fead18b2af03e1820734bff1f13b9362543
donggyu <leedong1848@gmail.com>
2023-05-02 15:01:07 +0900

    FEAT: auth Controller

---
+++ src/main/java/org/mint/smallcloud/security/controller/AuthController.java
@@ -0,0 +1,53 @@
+package org.mint.smallcloud.security.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.mint.smallcloud.security.UserDetailsProvider;
+import org.mint.smallcloud.security.dto.RegisterDto;
+import org.mint.smallcloud.security.dto.LoginDto;
+import org.mint.smallcloud.security.jwt.JwtTokenDto;
+import org.mint.smallcloud.security.jwt.JwtUserService;
+import org.mint.smallcloud.user.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/auth")
+@Slf4j
+@RequiredArgsConstructor
+public class AuthController {
+    private final JwtUserService jwtUserService;
+    private final UserService userService;
+    private final UserDetailsProvider userDetailsProvider;
+
+    @PostMapping("/login")
+    public JwtTokenDto login(@RequestBody LoginDto loginDto) {
+        return jwtUserService.login(loginDto);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> signup(@RequestBody RegisterDto registerDto) {
+        userService.registerUser(registerDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/refresh")
+    public JwtTokenDto refresh(HttpServletRequest request) {
+        /* TODO */
+        return JwtTokenDto.builder()
+                .grantType("")
+                .accessToken("")
+                .refreshToken("")
+                .build();
+    }
+
+    /* for test */
+    @GetMapping("/user")
+    public ResponseEntity<?> user() {
+        return ResponseEntity.ok(userDetailsProvider.getUserId());
+    }
+}
