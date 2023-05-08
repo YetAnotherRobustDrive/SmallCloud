4025203b5c31e935dce31152b317f214d5461292
JaeikBae <0308bae@naver.com>
2023-05-09 00:13:41 +0900

    for test

--- src/component/modal/ModalFileview.jsx
+++ src/component/modal/ModalFileview.jsx
@@ -1,15 +1,33 @@
 
 import '../../css/fileview.css'
+import DeregisterUser from '../../services/user/DeregisterUser';
+import ElevateUser from '../../services/user/ElevateUser';
 
 export default function ModalFileview(props) {
     const fileData = props.file;
-    
+
+    async function tmp1() {
+        const [isOk, message] = await ElevateUser('qwer');
+        console.log(isOk);
+        console.log(message);
+    }
+
+    async function tmp2() {
+        const [isOk, message] = await DeregisterUser();
+        console.log(isOk);
+        console.log(message);
+    }
+
     return (
         <div className="fileview">
             <div className='fileviewHead'>
                 <span className='filename'>nameeeeeeeeeeeeeeeeeeeeeee</span>
                 <div className='closeBtn'>열기</div>
             </div>
+            <Link to="/login">로그인</Link>
+            <button onClick={tmp1}>ElevateUser</button>
+            <button onClick={tmp2}>DeregisterUser</button>
+            <button onClick={() => props.after()}>닫기</button>
         </div>
     )
 
