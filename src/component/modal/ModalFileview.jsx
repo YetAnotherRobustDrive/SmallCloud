7e7bd362bfb47d8273cb4e95d8d8dfc2b59b3ec2
JaeikBae <0308bae@gmail.com>
2023-05-08 14:22:47 +0900

    add fileview

---
+++ src/component/modal/ModalFileview.jsx
@@ -0,0 +1,16 @@
+
+import '../../css/fileview.css'
+
+export default function ModalFileview(props) {
+    const fileData = props.file;
+    
+    return (
+        <div className="fileview">
+            <div className='fileviewHead'>
+                <span className='filename'>nameeeeeeeeeeeeeeeeeeeeeee</span>
+                <div className='closeBtn'>열기</div>
+            </div>
+        </div>
+    )
+
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -10,10 +10,13 @@ import UploadBtn from "../component/main/UploadBtn";
 import ListBox from "../component/main/ListBox";
 
 import datas from '../fakeJSON/direcFiles.json'
+import ModalFileview from "../component/modal/ModalFileview";
 
 export default function MainPage() {
 
     const [isGrid, setIsGrid] = useState(true);
+    const [isFileOpen, setIsFileOpen] = useState(true);
+    const [selected, setSelected] = useState();
 
     return (
         <>
@@ -24,12 +27,12 @@ export default function MainPage() {
                 <NarrowBox>
                     {
                         datas.map((data) => {
-                            return <CustomIcon 
-                            key={data.id}
-                            name={data.name}
-                            type={data.type} 
-                            stage={data.writingStage} 
-                            secu={data.securityLevel} />
+                            return <CustomIcon
+                                key={data.id}
+                                name={data.name}
+                                type={data.type}
+                                stage={data.writingStage}
+                                secu={data.securityLevel} />
                         })
                     }
                 </NarrowBox>
@@ -38,28 +41,34 @@ export default function MainPage() {
                     <GridBox height="calc(100vh - 299px)">
                         {
                             datas.map((data) => {
-                                return <CustomIcon 
-                                key={data.id}
-                                name={data.name}
-                                type={data.type} 
-                                stage={data.writingStage} 
-                                secu={data.securityLevel} />
+                                return <CustomIcon
+                                    key={data.id}
+                                    name={data.name}
+                                    type={data.type}
+                                    stage={data.writingStage}
+                                    secu={data.securityLevel} />
                             })
                         }
                     </GridBox>
                 }
                 {!isGrid &&
                     <>
-                    <div className="listscroll" style={{ height: "calc(100vh - 299px)" }}>{
-                        datas.map((data) => {
-                            return <ListBox key={data.id} data={data} />
-                        })
-                    }
-                    </div>
+                        <div className="listscroll" style={{ height: "calc(100vh - 299px)" }}>{
+                            datas.map((data) => {
+                                setSelected(data);
+                                return <ListBox key={data.id} data={data} />
+                            })
+                        }
+                        </div>
                     </>
                 }
                 <UploadBtn />
             </BodyFrame>
+            {isFileOpen &&
+                <ModalFileview 
+                file={selected} 
+                after={() => setIsFileOpen(false)}/>
+            }
         </>
     )
 }
