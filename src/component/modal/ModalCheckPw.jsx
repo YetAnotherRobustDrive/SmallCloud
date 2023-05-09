1f2008da8827ebbf6288b19ca57f1cadd4c8918c
JaeikBae <0308bae@naver.com>
2023-05-10 00:07:19 +0900

    add login link

--- src/component/modal/ModalCheckPw.jsx
+++ src/component/modal/ModalCheckPw.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Modal from 'react-modal';
+import { useNavigate } from 'react-router-dom'
 import default_profile_img from '../../img/defalutProfile.png';
 import ElevateUser from '../../services/user/ElevateUser';
+import ModalOk from "./ModalOk";
 
 export default function ModalCheckPW(props) {
 
@@ -10,6 +12,7 @@ export default function ModalCheckPW(props) {
     const [message, setMessage] = useState(false);
     const [nickname, setNickname] = useState("Nick_name");
     const [img, setImg] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (img === null) {
@@ -31,12 +34,15 @@ export default function ModalCheckPW(props) {
 
     const handleSubmit = async (e) => {
         if (e.key == 'Enter') {
-            const [isOk, messsage] = await ElevateUser(e.target.value);
+            const [isOk, message] = await ElevateUser(e.target.value);
             if (!isOk) { //fail
                 setCount(count + 1);
-                setMessage(messsage);
+                setMessage(message);
+                if (message == 'JWT토큰이 올바르지 않습니다.') {
+                    window.alert('로그인이 필요합니다.');
+                    navigate('/login');
+                }
                 setIsError(true);
-                
                 return;
             }
             setCount(0);
@@ -46,21 +52,23 @@ export default function ModalCheckPW(props) {
 
     Modal.setAppElement("#root");
     return (
-        <Modal isOpen={props.isOpen} style={modalStyle}>
-            <div className="modalOuter">
-                <img src={img}></img>
-                <span className="nick">{nickname}</span>
-                <input
-                    type="password"
-                    placeholder="PW"
-                    onKeyDown={handleSubmit}
-                    autoFocus
-                />
-                <span>보안을 위해 비밀번호를 입력해주세요.</span>
-                {isError &&
-                    <span style={{ color: "red", paddingTop: "5px", textDecoration: "underline" }}>{message + "(" + count + ")"}</span>
-                }
-            </div>
-        </Modal>
+        <>
+            <Modal isOpen={props.isOpen} style={modalStyle}>
+                <div className="modalOuter">
+                    <img src={img}></img>
+                    <span className="nick">{nickname}</span>
+                    <input
+                        type="password"
+                        placeholder="PW"
+                        onKeyDown={handleSubmit}
+                        autoFocus
+                    />
+                    <span>보안을 위해 비밀번호를 입력해주세요.</span>
+                    {isError &&
+                        <span style={{ color: "red", paddingTop: "5px", textDecoration: "underline" }}>{message + "(" + count + ")"}</span>
+                    }
+                </div>
+            </Modal>
+        </>
     )
 }
