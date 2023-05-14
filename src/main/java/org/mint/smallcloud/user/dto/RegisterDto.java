9fc761f824eb691392b35999f442011377aa565e
donggyu <leedong1848@gmail.com>
2023-05-14 19:26:16 +0900

    Add: Validate

--- src/main/java/org/mint/smallcloud/user/dto/RegisterDto.java
+++ src/main/java/org/mint/smallcloud/user/dto/RegisterDto.java
@@ -17,10 +17,12 @@ public class RegisterDto {
     @NotBlank(message = "이름은 필수로 들어가야 합니다.")
     @Pattern(regexp = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]+", message = "이름은 영어나 숫자나 한글만 가능합니다.")
     private final String name;
+
     @Size(min = 1, max = 15, message = "아이디는 15자 이하로 작성해 주세요")
     @NotBlank(message = "아이디는 필수로 들어가야 합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]+", message = "아이디는 영어나 숫자만 가능합니다.")
     private final String id;
+
     @Size(min = 1, max = 15, message = "비밀번호는 15이하로 작성해 주세요")
     @NotBlank(message = "비밀번호는 필수로 들어가야 합니다.")
     private final String password;
