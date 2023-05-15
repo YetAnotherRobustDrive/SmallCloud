808f432525801117befbb754d6099a036bbbfe52
donggyu <leedong1848@gmail.com>
2023-05-16 00:09:07 +0900

    TEST: 테스트 추가

--- src/main/java/org/mint/smallcloud/file/domain/FileLocation.java
+++ src/main/java/org/mint/smallcloud/file/domain/FileLocation.java
@@ -6,13 +6,15 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Getter
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FileLocation implements Serializable {
-    @Column(name = "LOCATION")
+    @Column(name = "LOCATION", length = 40)
+    @Size(min = 1, max = 40, message = "파일 로키이션은 40자 이하로 작성해 주세요")
     private String location;
 
     protected FileLocation(String location) {
