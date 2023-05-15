8656fb96d0d89dcf2d746ed4c8ebfedd5a34212f
donggyu <leedong1848@gmail.com>
2023-05-15 12:41:40 +0900

    FEAT: member profile에 group 추가

---
+++ src/main/java/org/mint/smallcloud/group/service/GroupThrowerService.java
@@ -0,0 +1,26 @@
+package org.mint.smallcloud.group.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.mint.smallcloud.exception.ExceptionStatus;
+import org.mint.smallcloud.exception.ServiceException;
+import org.mint.smallcloud.group.domain.Group;
+import org.mint.smallcloud.group.repository.GroupRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class GroupThrowerService {
+    private final GroupRepository groupRepository;
+
+    public Group getGroupByName(String groupName) {
+        return groupRepository.findByName(groupName)
+            .orElseThrow(() -> new ServiceException(ExceptionStatus.NOT_FOUND_GROUP_NAME));
+    }
+
+    public void checkExistedGroupName(String groupName) {
+        if (!groupRepository.existsByName(groupName))
+            throw new ServiceException(ExceptionStatus.NOT_FOUND_GROUP_NAME);
+    }
+}
