6d66e0bcecbc5b3feb20f1c1160732e4191390a8
donggyu <leedong1848@gmail.com>
2023-05-16 15:59:38 +0900

    FORCE: 기각

--- src/test/java/org/mint/smallcloud/board/controller/BoardControllerTest.java
+++ src/test/java/org/mint/smallcloud/board/controller/BoardControllerTest.java
@@ -8,12 +8,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mint.smallcloud.TestSnippet;
 import org.mint.smallcloud.board.domain.Board;
 import org.mint.smallcloud.board.dto.BoardDto;
-import org.mint.smallcloud.board.repository.BoardRepository;
 import org.mint.smallcloud.security.dto.UserDetailsDto;
 import org.mint.smallcloud.security.jwt.dto.JwtTokenDto;
 import org.mint.smallcloud.security.jwt.tokenprovider.JwtTokenProvider;
 import org.mint.smallcloud.user.domain.Member;
-import org.mint.smallcloud.user.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -48,12 +46,6 @@ class BoardControllerTest {
     private ObjectMapper objectMapper;
 
     @Autowired
-    private BoardRepository boardRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
 
@@ -63,11 +55,11 @@ class BoardControllerTest {
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders
-                .webAppContextSetup(webApplicationContext)
-                .addFilters(new CharacterEncodingFilter("UTF-8", true))
-                .apply(documentationConfiguration(restDocumentation))
-                .apply(springSecurity())
-                .build();
+            .webAppContextSetup(webApplicationContext)
+            .addFilters(new CharacterEncodingFilter("UTF-8", true))
+            .apply(documentationConfiguration(restDocumentation))
+            .apply(springSecurity())
+            .build();
     }
 
     @Test
@@ -77,54 +69,54 @@ class BoardControllerTest {
         Member member = Member.createCommon("testMemberName", "testPw", "testNickname");
         memberRepository.save(member);
         UserDetailsDto userDetailsDto = UserDetailsDto.builder()
-                .username(member.getUsername())
-                .password(member.getPassword())
-                .disabled(member.isLocked())
-                .roles(member.getRole())
-                .build();
+            .username(member.getUsername())
+            .password(member.getPassword())
+            .disabled(member.isLocked())
+            .roles(member.getRole())
+            .build();
         JwtTokenDto memberToken = jwtTokenProvider.generateTokenDto(userDetailsDto);
 
         RequestFieldsSnippet payload = requestFields(
-                fieldWithPath("content").description("사용자 질문의 내용을 담고 있습니다."),
-                fieldWithPath("contact").description("사용자의 연락처를 담고 있습니다."),
-                fieldWithPath("writer").description("글의 작성자를 담고 있습니다."));
+            fieldWithPath("content").description("사용자 질문의 내용을 담고 있습니다."),
+            fieldWithPath("contact").description("사용자의 연락처를 담고 있습니다."),
+            fieldWithPath("writer").description("글의 작성자를 담고 있습니다."));
         BoardDto securedBoardDto =
-                BoardDto.builder()
-                        .content("testContent")
-                        .contact("testContact")
-                        .writer("testWriter")
-                        .build();
+            BoardDto.builder()
+                .content("testContent")
+                .contact("testContact")
+                .writer("testWriter")
+                .build();
 
         BoardDto boardDto =
-                BoardDto.builder()
-                        .content("testContent1")
-                        .contact("testContact1")
-                        .build();
+            BoardDto.builder()
+                .content("testContent1")
+                .contact("testContact1")
+                .build();
 
         BoardDto notContactBoardDto =
-                BoardDto.builder()
-                        .content("testContent1")
-                        .build();
+            BoardDto.builder()
+                .content("testContent1")
+                .build();
 
         // 정상적 1:1 문의 등록
         this.mockMvc.perform(TestSnippet.securePost(url, memberToken.getAccessToken(), objectMapper, securedBoardDto))
-                .andExpect(status().isOk())
-                .andDo(document("OneToOneRegisterInquiry",payload));
+            .andExpect(status().isOk())
+            .andDo(document("OneToOneRegisterInquiry", payload));
 
         // 정상적 로그인 문의 등록
         this.mockMvc.perform(TestSnippet.post(url, objectMapper, boardDto))
-                .andExpect(status().isOk())
-                .andDo(document("LoginRegisterInquiry",payload));
+            .andExpect(status().isOk())
+            .andDo(document("LoginRegisterInquiry", payload));
 
         // 연락처가 없음 - 로그인 문의 등록
         this.mockMvc.perform(TestSnippet.post(url, objectMapper, notContactBoardDto))
-                .andExpect(status().isBadRequest())
-                .andDo(document("NoContactInquiry",payload));
+            .andExpect(status().isBadRequest())
+            .andDo(document("NoContactInquiry", payload));
 
         // 잘못된 토큰 - 1:1 문의 등록
         this.mockMvc.perform(TestSnippet.securePost(url, "testWrongToken", objectMapper, securedBoardDto))
-                .andExpect(status().isBadRequest())
-                .andDo(document("WrongTokenSaveInquiry",payload));
+            .andExpect(status().isBadRequest())
+            .andDo(document("WrongTokenSaveInquiry", payload));
     }
 
 
@@ -135,31 +127,31 @@ class BoardControllerTest {
         Member admin = Member.createAdmin("testAdminName", "testPw", "testNickname");
         memberRepository.save(admin);
         UserDetailsDto adminDto = UserDetailsDto.builder()
-                .username(admin.getUsername())
-                .password(admin.getPassword())
-                .disabled(admin.isLocked())
-                .roles(admin.getRole())
-                .build();
+            .username(admin.getUsername())
+            .password(admin.getPassword())
+            .disabled(admin.isLocked())
+            .roles(admin.getRole())
+            .build();
         JwtTokenDto adminToken = jwtTokenProvider.generateTokenDto(adminDto);
 
-        Board board = Board.board("testContent","testContact");
-        Board board1 = Board.board("testContent1","testContact1");
+        Board board = Board.board("testContent", "testContact");
+        Board board1 = Board.board("testContent1", "testContact1");
 
         boardRepository.save(board);
         boardRepository.save(board1);
         List<Board> findBoard = boardRepository.findAll();
 
-        this.mockMvc.perform(TestSnippet.secureGet(url,adminToken.getAccessToken()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0].content").value(findBoard.get(0).getContent()))
-                .andExpect(jsonPath("$.[0].contact").value(findBoard.get(0).getContact()))
-                .andExpect(jsonPath("$.[1].content").value(findBoard.get(1).getContent()))
-                .andExpect(jsonPath("$.[1].contact").value(findBoard.get(1).getContact()))
-                .andDo(document("GetAllInquiries"));
+        this.mockMvc.perform(TestSnippet.secureGet(url, adminToken.getAccessToken()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.[0].content").value(findBoard.get(0).getContent()))
+            .andExpect(jsonPath("$.[0].contact").value(findBoard.get(0).getContact()))
+            .andExpect(jsonPath("$.[1].content").value(findBoard.get(1).getContent()))
+            .andExpect(jsonPath("$.[1].contact").value(findBoard.get(1).getContact()))
+            .andDo(document("GetAllInquiries"));
 
         this.mockMvc.perform(TestSnippet.secureGet(url, adminToken.getAccessToken()))
-                .andExpect(status().isOk())
-                .andDo(document("CannotFindAllBoardContent"));
+            .andExpect(status().isOk())
+            .andDo(document("CannotFindAllBoardContent"));
     }
 
     @Test
@@ -169,15 +161,15 @@ class BoardControllerTest {
         Member admin = Member.createAdmin("testUsername", "testPw", "testNickname");
         memberRepository.save(admin);
         UserDetailsDto adminDto = UserDetailsDto.builder()
-                .username(admin.getUsername())
-                .password(admin.getPassword())
-                .disabled(admin.isLocked())
-                .roles(admin.getRole())
-                .build();
+            .username(admin.getUsername())
+            .password(admin.getPassword())
+            .disabled(admin.isLocked())
+            .roles(admin.getRole())
+            .build();
         JwtTokenDto adminToken = jwtTokenProvider.generateTokenDto(adminDto);
 
-        Board board = Board.board("testContent","testContact");
-        Board board1 = Board.board("testContent1","testContact1");
+        Board board = Board.board("testContent", "testContact");
+        Board board1 = Board.board("testContent1", "testContact1");
 
         boardRepository.save(board);
         boardRepository.save(board1);
@@ -186,23 +178,23 @@ class BoardControllerTest {
         String expectByContent = "$[%s].content";
         String expectByContact = "$[%s].contact";
 
-        this.mockMvc.perform(TestSnippet.secureGet(url,adminToken.getAccessToken()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath(expectByContent, 0).value(findBoard.get().getContent()))
-                .andExpect(jsonPath(expectByContact, 0).value(findBoard.get().getContact()))
-                .andDo(document("GetInquiry"));
+        this.mockMvc.perform(TestSnippet.secureGet(url, adminToken.getAccessToken()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath(expectByContent, 0).value(findBoard.get().getContent()))
+            .andExpect(jsonPath(expectByContact, 0).value(findBoard.get().getContact()))
+            .andDo(document("GetInquiry"));
 
-        this.mockMvc.perform(TestSnippet.secureGet(url,adminToken.getAccessToken()))
-                .andExpect(status().isForbidden())
-                .andExpect(jsonPath(expectByContent, 2).value(findBoard.get().getContent()))
-                .andExpect(jsonPath(expectByContact, 2).value(findBoard.get().getContact()))
-                .andDo(document("CannotFindInquiry"));
+        this.mockMvc.perform(TestSnippet.secureGet(url, adminToken.getAccessToken()))
+            .andExpect(status().isForbidden())
+            .andExpect(jsonPath(expectByContent, 2).value(findBoard.get().getContent()))
+            .andExpect(jsonPath(expectByContact, 2).value(findBoard.get().getContact()))
+            .andDo(document("CannotFindInquiry"));
 
         this.mockMvc.perform(TestSnippet.secureGet(url, "testWrongToken"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath(expectByContent, 0).value(findBoard.get().getContent()))
-                .andExpect(jsonPath(expectByContact, 0).value(findBoard.get().getContact()))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath(expectByContent, 0).value(findBoard.get().getContent()))
+            .andExpect(jsonPath(expectByContact, 0).value(findBoard.get().getContact()))
 
-                .andDo(document("WrongTokenGetInquiry"));
+            .andDo(document("WrongTokenGetInquiry"));
     }
 }
