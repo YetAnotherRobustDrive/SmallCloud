9de405291802abcad4f526fbd07342110ac3ef53
JaeikBae <0308bae@gmail.com>
2023-05-17 10:48:31 +0900

    update container name

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker stop yard_test; docker build -t yard_test_image . && docker run -v $(pwd)/src:/app/src:ro -p 3000:3000 -d --rm --name=yard_test yard_test_image
+docker stop smallcloud-frontend; 
+docker build -t frontend . && docker run -v $(pwd)/src:/app/src:ro -p 3000:3000 -d --rm --name=frontend smallcloud-frontend
