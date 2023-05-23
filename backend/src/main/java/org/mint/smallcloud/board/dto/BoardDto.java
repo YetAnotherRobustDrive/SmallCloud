6d8534005573f6fc30339624b0059a60f866172b
kindbear <dlgkdrb2@naver.com>
2023-05-23 20:19:32 +0900

    FEAT: board 등록

--- backend/src/main/java/org/mint/smallcloud/board/dto/BoardDto.java
+++ backend/src/main/java/org/mint/smallcloud/board/dto/BoardDto.java
@@ -3,6 +3,7 @@ package org.mint.smallcloud.board.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.mint.smallcloud.board.domain.BoardType;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -11,17 +12,10 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @Builder
 public class BoardDto {
-//
-//    private  final String title;
-//
-//    @Size(min = 1)
-//    @NotBlank(message = "연락처는 필수로 들어가야 합니다.")
-//    private final String contact;
-//
-//    @Size(min = 1, max = 500, message = "문의 내용은 500자 이하로 작성해 주세요.")
-//    @NotBlank(message = "문의 내용은 필수로 들어가야 합니다.")
-//    private final String content;
-//
-//    private final String writer;
+    private final String title;
+
+    private final String content;
+
+    private final BoardType boardType;
 }
 
