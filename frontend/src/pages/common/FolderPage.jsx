edd9862258ef411de5ebb48918451523c6e9a704
JaeikBae <0308bae@gmail.com>
2023-05-29 18:17:01 +0900

    (FE)FEAT: 파일보기 파일 이름 추가

--- frontend/src/pages/common/FolderPage.jsx
+++ frontend/src/pages/common/FolderPage.jsx
@@ -10,8 +10,9 @@ import UploadBtn from "../../component/main/UploadBtn";
 import ModalFileview from "../../component/modal/ModalFileview";
 import ModalLoading from "../../component/modal/ModalLoading";
 import Sidebar from "../../component/sidebar/Sidebar";
-
-import datas from '../../fakeJSON/direcFiles.json';
+import GetSubDirList from "../../services/directory/GetSubDirList";
+import GetSubFileList from "../../services/directory/GetSubFileList";
+import GetDirInfo from "../../services/directory/GetDirInfo";
 
 export default function FolderPage() {
 
@@ -19,11 +20,39 @@ export default function FolderPage() {
     const [isFileView, setIsFileView] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [selected, setSelected] = useState();
+    const [isFail, setIsFail] = useState();
+    const [message, setMessage] = useState();
+    const [files, setFiles] = useState([]);
+    const [name, setName] = useState([]);
     const params = useParams();
 
     useEffect(() => {
-        setTimeout(() => setIsLoading(false), 500);
-    }, [])
+        const render = async () => {
+            const infoRes = await GetDirInfo(params.fileID);
+            if (!infoRes[0]) {
+                throw infoRes[1];
+            }
+            setName(infoRes[1].name);
+            const subFileRes = await GetSubFileList(params.fileID);
+            if (!subFileRes[0]) {
+                throw subFileRes[1];
+            }
+            const subDirRes = await GetSubDirList(params.fileID);
+            if (!subDirRes[0]) {
+                throw subDirRes[1];
+            }
+            setFiles([...subDirRes[1], ...subFileRes[1]]);
+        }
+        try {
+            setIsLoading(true);
+            render();
+            setTimeout(() => setIsLoading(false), 500);            
+        } catch (error) {
+            setIsFail(true);
+            setMessage(error);
+            setIsLoading(false);
+        }
+    }, [params.fileID])
 
     return (
         <>
@@ -31,11 +60,11 @@ export default function FolderPage() {
             <Header />
             <Sidebar />
             <BodyFrame hasContext={true}>
-                <BodyHeader text={params.fileID} addon={setIsGrid} view={isGrid} />
+                <BodyHeader text={name} addon={setIsGrid} view={isGrid} />
                 {isGrid &&
                     <GridBox height="calc(100vh - 117px)">
                         {
-                            datas.map((data) => {
+                            files.map((data) => {
                                 return <CustomIcon
                                     onClick={() => {
                                         setSelected(data);
@@ -54,7 +83,7 @@ export default function FolderPage() {
                 {!isGrid &&
                     <>
                         <div className="listscroll" style={{ height: "calc(100vh - 117px)" }}>{
-                            datas.map((data) => {
+                            files.map((data) => {
                                 return <ListBox
                                     key={data.id}
                                     onClick={() => {
