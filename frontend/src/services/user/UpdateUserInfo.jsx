7b3487b2d8ba2f75448d881182b31922b5fe758d
JaeikBae <0308bae@naver.com>
2023-06-05 08:29:28 +0900

    (FE)FEAT: 프로필이미지 등록 백엔드 연동

--- frontend/src/services/user/UpdateUserInfo.jsx
+++ frontend/src/services/user/UpdateUserInfo.jsx
@@ -12,18 +12,46 @@ export default async function UpdateUserInfo(value) {
         },
         body: value,
     };
+    const uploadImage = async () => {
+        const formData = new FormData();
+        formData.append('imageFile', value.location);
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', configData.API_SERVER + 'users/profile-photo', true);
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    if (xhr.status !== 200) {
+                        resolve(xhr.responseText);
+                    }
+                    else {
+                        const tRes = {
+                            "status": 200,
+                            "data": xhr.responseText
+                        }
+                        resolve(tRes);
+                    }
+                }
+            }
+            xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
+            xhr.send(formData);
+        });
+    }
 
     try {
-    
-        const userID = jwtDecode(accessToken).sub;
-
-        const res = await fetch(configData.API_SERVER + 'users/' + userID + '/update', model);
-        if (!res.ok) {
-            const data = await res.json();
-            throw data;
+        const imgRes = await uploadImage();
+        if (imgRes.status !== 200) {
+            throw imgRes;
         }
+
+        // const userID = jwtDecode(accessToken).sub;
+
+        // const res = await fetch(configData.API_SERVER + 'users/' + userID + '/update', model);
+        // if (!res.ok) {
+        //     const data = await res.json();
+        //     throw data;
+        // }
         return [true, ''];
     } catch (e) {
         return [false, e.message];
     }
-}
+ }
