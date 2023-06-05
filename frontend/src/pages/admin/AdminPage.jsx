87b7a1fc7af6b3a18b9a738fae4335973150eaf9
JaeikBae <0308bae@gmail.com>
2023-06-05 15:37:49 +0900

    (FE)FEAT: 어드민 승급 삭제

--- frontend/src/pages/admin/AdminPage.jsx
+++ frontend/src/pages/admin/AdminPage.jsx
@@ -2,13 +2,11 @@ import { useDispatch } from "react-redux";
 import { asyncCheckAdmin } from "../../slice/TokenSlice";
 import { useNavigate } from 'react-router-dom';
 import IsAdminToken from "../../services/token/IsAdminToken";
-import TESTADMIN from '../../services/admin/TESTADMIN'
 
 export default function AdminPage() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const render = async () => {
-        const test = await TESTADMIN();
         const isAdmin = await IsAdminToken();
         if (!isAdmin) { //fail
             navigate('/');
