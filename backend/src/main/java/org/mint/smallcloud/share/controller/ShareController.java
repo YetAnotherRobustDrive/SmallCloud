705f60edc30081b7985c44fb24e499ad5ae893e0
donggyu <leedong1848@gmail.com>
2023-06-06 16:18:06 +0900

    [BE] FEAT: datanode share list

--- backend/src/main/java/org/mint/smallcloud/share/controller/ShareController.java
+++ backend/src/main/java/org/mint/smallcloud/share/controller/ShareController.java
@@ -3,17 +3,17 @@ package org.mint.smallcloud.share.controller;
 import lombok.RequiredArgsConstructor;
 import org.mint.smallcloud.exception.ExceptionStatus;
 import org.mint.smallcloud.exception.ServiceException;
+import org.mint.smallcloud.file.dto.DirectoryDto;
+import org.mint.smallcloud.file.dto.FileDto;
 import org.mint.smallcloud.security.UserDetailsProvider;
 import org.mint.smallcloud.share.dto.ShareRequestDto;
 import org.mint.smallcloud.share.service.ShareService;
 import org.mint.smallcloud.user.domain.Roles;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/share")
@@ -37,5 +37,21 @@ public class ShareController {
         String username = userDetailsProvider
             .getUserDetails().orElseThrow(() -> new ServiceException(ExceptionStatus.NO_PERMISSION)).getUsername();
         shareService.delete(username, shareRequestDto);
+    }
+
+    @Secured(Roles.S_COMMON)
+    @GetMapping("/file-list")
+    public List<FileDto> getFileList() {
+        String username = userDetailsProvider
+            .getUserDetails().orElseThrow(() -> new ServiceException(ExceptionStatus.NO_PERMISSION)).getUsername();
+        return shareService.getFileList(username);
+    }
+
+    @Secured(Roles.S_COMMON)
+    @GetMapping("/directory-list")
+    public List<DirectoryDto> getDirectoryList() {
+        String username = userDetailsProvider
+            .getUserDetails().orElseThrow(() -> new ServiceException(ExceptionStatus.NO_PERMISSION)).getUsername();
+        return shareService.getDirectoryList(username);
     }
 }
