499fac1f54d20d56c1b9abb7ad9de68f234ddc72
kindbear <dlgkdrb2@naver.com>
2023-06-08 21:20:24 +0900

    [BE] FIX: file labelUpdate 수정, label return size 추가

--- backend/src/main/java/org/mint/smallcloud/file/repository/FileRepository.java
+++ backend/src/main/java/org/mint/smallcloud/file/repository/FileRepository.java
@@ -2,12 +2,16 @@ package org.mint.smallcloud.file.repository;
 
 import org.mint.smallcloud.file.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
     Optional<File> findByFileType_NameAndParentFolder_Id(String name, Long parentFolderId);
 
+    @Query("select f from File f join f.labels l where l.name = :labelName and l.owner.nickname = :ownerName")
+    List<File> findDataNodeByLabelNameAndOwner(String labelName, String ownerName);
 }
