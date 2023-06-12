b4114d334452af0d219076d6afeab1a692473db3
JaeikBae <0308bae@naver.com>
2023-06-13 01:48:52 +0900

    [FE] FEAT: 개인정보, 약관 변경 내역 확인 추가

--- frontend/src/pages/mypage/ServiceInfoPage.jsx
+++ frontend/src/pages/mypage/ServiceInfoPage.jsx
@@ -1,14 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../component/header/Header";
 import SidebarMypage from "../../component/sidebar/SidebarMypage";
+import BodyFrame from "../../component/Bodyframe";
+import BodyHeader from "../../component/main/BodyHeader";
+import ReacDiffViewer from "react-diff-viewer";
+import GetBoardListFrom from "../../services/board/GetBoardListFrom";
 
-//todo
 export default function ServiceInfoPage() {
 
+    const [lastTwoTerm, setLastTwoTerm] = useState([]);
+    const [lastTwoPrivacy, setLastTwoPrivacy] = useState([]);
+
+    useEffect(() => {
+        const renderPrivacy = async () => {
+            const privatesNew = await GetBoardListFrom("inquiries/board/created?boardType=privacy&createdDate=0");
+            const privatesOld = await GetBoardListFrom("inquiries/board/created?boardType=privacy&createdDate=1");
+            if (privatesNew[1] === null) {
+                setLastTwoPrivacy(["", ""]);
+                return;
+            }
+            if (privatesOld[1] === null) {
+                setLastTwoPrivacy(["", privatesNew[1].content]);
+                return;
+            }
+            setLastTwoPrivacy([privatesOld[1].content, privatesNew[1].content]);
+        }
+        const renderTerms = async () => {
+            const termNew = await GetBoardListFrom("inquiries/board/created?boardType=terms&createdDate=0");
+            const termOld = await GetBoardListFrom("inquiries/board/created?boardType=terms&createdDate=1");
+            if (termNew[1] === null) {
+                setLastTwoTerm(["", ""]);
+                return;
+            }
+            else if (termOld[1] === null) {
+                setLastTwoTerm(["", termNew[1].content]);
+                return;
+            }
+            setLastTwoTerm([termOld[1].content, termNew[1].content]);
+        }
+        renderPrivacy();
+        renderTerms();
+    }, [])
+    
     return (
         <>
             <Header />
             <SidebarMypage />
+            <BodyFrame>
+                <BodyHeader text="약관 변경내역 (이전 / 최신)" />
+                {(lastTwoTerm[0] === "" || lastTwoTerm[1] === "") ? <div>이전 버전이 없습니다.</div> :
+                    <ReacDiffViewer
+                        oldValue={lastTwoTerm[0]}
+                        newValue={lastTwoTerm[1]}
+                        splitView={true}
+                    />
+                }
+                <BodyHeader text="개인정보 취급 방침 변경내역 (이전 / 최신)" />
+                {(lastTwoPrivacy[0] === "" || lastTwoPrivacy[1] === "") ? <div>이전 버전이 없습니다.</div> :
+                    <ReacDiffViewer
+                        oldValue={lastTwoPrivacy[0]}
+                        newValue={lastTwoPrivacy[1]}
+                        splitView={true}
+                    />
+                }
+
+
+            </BodyFrame>
         </>
     )
 }
