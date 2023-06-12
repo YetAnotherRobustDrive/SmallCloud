679c7d2d0a3e4d351b4fa2f89696a59b77c4b229
JaeikBae <0308bae@naver.com>
2023-06-13 01:16:33 +0900

    [FE] FEAT: 유저 로그인 로그 관련 추가

--- frontend/src/pages/mypage/SecurityInfoPage.jsx
+++ frontend/src/pages/mypage/SecurityInfoPage.jsx
@@ -1,14 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../component/header/Header";
 import SidebarMypage from "../../component/sidebar/SidebarMypage";
+import BodyFrame from "../../component/Bodyframe";
+import BodyHeader from "../../component/main/BodyHeader";
+import GetLoginLog from "../../services/log/GetLoginLog";
 
-//todo
 export default function SecurityInfoPage() {
+
+    const [log, setLog] = useState([
+        {
+            time: "2021-08-01 12:00:00",
+            user: "admin",
+            action: "로그인",
+            status: "성공",
+            ip: "111.111.111.111",
+        },
+    ]);
+
+    useEffect(() => {
+        // const init = async () => {
+        //     const res = await GetLoginLog();
+        //     setLog(res);
+        // }
+        // init();
+    }, []);
 
     return (
         <>
             <Header />
             <SidebarMypage />
+            <BodyFrame>
+                <BodyHeader text="로그인 내역" />
+                <div className="logTable">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>시간</th>
+                                <th>닉네임</th>
+                                <th>종류</th>
+                                <th>결과</th>
+                                <th>IP</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                log.map((item, index) => (
+                                    <tr key={index}>
+                                        <td>{item.time}</td>
+                                        <td>{item.user}</td>
+                                        <td>{item.action}</td>
+                                        <td>{item.status}</td>
+                                        <td>{item.ip}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </BodyFrame>
         </>
     )
 }
---
+++ frontend/src/services/log/GetLoginLog.jsx
@@ -0,0 +1,26 @@
+import RefreshToken from "../token/RefreshToken";
+import configData from "../../config/config.json"
+
+export default async function GetLoginLog() {
+    await RefreshToken();
+    const accessToken = localStorage.getItem("accessToken");
+    const model = {
+        method: "GET",
+        headers: {
+            "Authorization": "Bearer " + accessToken,
+        },
+    };
+
+    try {
+        const res = await fetch(configData.API_SERVER + 'logs', model);
+        const data = await res.json();
+        if (res.status === 200) {
+            return [true, data];  //성공
+        }
+        else {
+            throw data; //실패
+        }
+    } catch (e) {
+        return [false, e.message]; //실패 후 처리
+    }
+}
