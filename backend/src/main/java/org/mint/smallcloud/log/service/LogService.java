275012a5bc524318089b2b6e56765b4054d5d270
donggyu <leedong1848@gmail.com>
2023-06-14 22:58:44 +0900

    [BE] FIX: jpql query

--- backend/src/main/java/org/mint/smallcloud/log/service/LogService.java
+++ backend/src/main/java/org/mint/smallcloud/log/service/LogService.java
@@ -8,7 +8,6 @@ import org.mint.smallcloud.log.dto.ResponseLoginLogDto;
 import org.mint.smallcloud.log.mapper.LogMapper;
 import org.mint.smallcloud.log.user.UserLog;
 import org.mint.smallcloud.log.user.UserLogRepository;
-import org.mint.smallcloud.user.service.MemberThrowerService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -23,7 +22,6 @@ import java.util.stream.Collectors;
 @Transactional
 public class LogService {
     private final UserLogRepository userLogRepository;
-    private final MemberThrowerService memberThrowerService;
     private final LogMapper logMapper;
 
     public Page<ResponseLogDto> findLogs(RequestLogDto requestLogDto, Pageable pageable) {
@@ -50,7 +48,7 @@ public class LogService {
         List<UserLog> userLogs = userLogRepository.findByActionStartsWith("/ping/login/" + username + "/");
         List<UserLog> formatted = userLogs.stream()
                 .map(e -> {
-                    Boolean status = e.getAction().contains("success") ? true : false;
+                    Boolean status = e.getAction().contains("success");
                     String action = e.getAction().replace("/ping/", "").replace("success", "").replace("fail", "");
                     return UserLog.of(
                             e.getMember(),
--- backend/src/main/java/org/mint/smallcloud/log/user/UserLogRepository.java
+++ backend/src/main/java/org/mint/smallcloud/log/user/UserLogRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -23,79 +24,59 @@ public class UserLogRepository {
     }
 
     public Page<UserLog> findLogs(String nickname, LocalDateTime startTime, LocalDateTime endTime, Boolean status, String action, Pageable pageable) {
-        String jpql = "select ul from UserLog ul join ul.member m";
-        boolean isFirst = true;
-        if (nickname != null) {
-            if (isFirst) {
-                jpql += " where";
-                isFirst = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " m.nickname = :nickname";
-        }
-        if (startTime != null && endTime == null) {
-            if (isFirst) {
-                jpql += " where";
-                isFirst = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " ul.time >= :startTime";
-        }
-        if (startTime == null && endTime != null) {
-            if (isFirst) {
-                jpql += " where";
-                isFirst = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " ul.time <= :endTime";
-        }
-        if (startTime != null && endTime != null) {
-            if (isFirst) {
-                jpql += " where";
-                isFirst = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " ul.time between :startTime and :endTime";
-        }
-        if (status != null) {
-            if (isFirst) {
-                jpql += " where";
-                isFirst = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " ul.status = :status";
-        }
-        if (action != null) {
-            if (isFirst) {
-                jpql += " where";
-                isFirst = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " ul.action like :action";
-        }
-
-        TypedQuery<UserLog> query = em.createQuery(jpql, UserLog.class);
+        String selectJpql = "select ul";
+        String countingJpqlSelect = "select count(ul)";
+        String fromJpql = " from UserLog ul join ul.member m";
+        List<String> conditionsWhereJpql = new ArrayList<>();
+        // 처음 -> where condition, 이후 -> and condition
+        // 조건들을 list에 담아서 처리하는 방법도 있음
+        // list를 담을 때 where 조건이면 처음에 넣고, and 조건이면 뒤에 넣는다.
+        // A, B 조건이 있을 때, A 조건이면 where A, B 조건이면 where B and A
+        // 1. total count
+        // 2. result (contain offset, limit)
+        if (nickname != null)
+            conditionsWhereJpql.add("m.nickname = :nickname");
+        if (startTime != null && endTime == null)
+            conditionsWhereJpql.add("ul.time >= :startTime");
+        if (startTime == null && endTime != null)
+            conditionsWhereJpql.add("ul.time <= :endTime");
+        if (startTime != null && endTime != null)
+            conditionsWhereJpql.add("ul.time between :startTime and :endTime");
+        if (status != null)
+            conditionsWhereJpql.add("ul.status = :status");
+        if (action != null)
+             conditionsWhereJpql.add("ul.action like :action");
+        String orderByJpql = "order by ul.time desc";
+        String whereJpql = conditionsWhereJpql.isEmpty() ? "" : " where " + String.join(" and ", conditionsWhereJpql);
+        TypedQuery<UserLog> query = em.createQuery(
+                selectJpql + fromJpql + whereJpql + orderByJpql,
+                UserLog.class)
+            .setFirstResult((int) pageable.getOffset())
+            .setMaxResults(pageable.getPageSize());
+        TypedQuery<Long> countingQuery= em.createQuery(
+            countingJpqlSelect + fromJpql + whereJpql,
+            Long.class);
         if (nickname != null) {
             query = query.setParameter("nickname", nickname);
+            countingQuery = countingQuery.setParameter("nickname", nickname);
         }
         if (startTime != null && endTime == null) {
             query = query.setParameter("startTime", startTime);
+            countingQuery = countingQuery.setParameter("startTime", startTime);
         }
         if (startTime == null && endTime != null) {
             query = query.setParameter("endTime", endTime);
+            countingQuery = countingQuery.setParameter("endTime", endTime);
         }
         if (startTime != null && endTime != null) {
             query = query.setParameter("startTime", startTime);
             query = query.setParameter("endTime", endTime);
+            countingQuery = countingQuery.setParameter("startTime", startTime);
+            countingQuery = countingQuery.setParameter("endTime", endTime);
         }
         if (status != null) {
             query = query.setParameter("status", status);
+            countingQuery = countingQuery.setParameter("status", status);
         }
         if (action != null) {
             // convert
@@ -103,9 +84,10 @@ public class UserLogRepository {
             // to /group/%/remove-user/%
             String actionLike = action.replaceAll("\\{.*?\\}", "%");
             query = query.setParameter("action", actionLike);
+            countingQuery = countingQuery.setParameter("action", actionLike);
         }
         List<UserLog> content = query.getResultList();
-        long total = query.getResultList().size();
+        Long total = countingQuery.getSingleResult();
         return new PageImpl<>(content, pageable, total);
     }
 
