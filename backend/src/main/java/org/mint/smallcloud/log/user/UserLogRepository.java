64dfb515a6000b2a6aedc0c00f2abd1ed4d87a96
donggyu <leedong1848@gmail.com>
2023-06-15 11:37:32 +0900

    [BE] FIX: fix

--- backend/src/main/java/org/mint/smallcloud/log/user/UserLogRepository.java
+++ backend/src/main/java/org/mint/smallcloud/log/user/UserLogRepository.java
@@ -27,13 +27,8 @@ public class UserLogRepository {
         String selectJpql = "select ul";
         String countingJpqlSelect = "select count(ul)";
         String fromJpql = " from UserLog ul join ul.member m";
+        String orderByJpql = " order by ul.time desc";
         List<String> conditionsWhereJpql = new ArrayList<>();
-        // 처음 -> where condition, 이후 -> and condition
-        // 조건들을 list에 담아서 처리하는 방법도 있음
-        // list를 담을 때 where 조건이면 처음에 넣고, and 조건이면 뒤에 넣는다.
-        // A, B 조건이 있을 때, A 조건이면 where A, B 조건이면 where B and A
-        // 1. total count
-        // 2. result (contain offset, limit)
         if (nickname != null)
             conditionsWhereJpql.add("m.nickname = :nickname");
         if (startTime != null && endTime == null)
@@ -46,13 +41,14 @@ public class UserLogRepository {
             conditionsWhereJpql.add("ul.status = :status");
         if (action != null)
              conditionsWhereJpql.add("ul.action like :action");
-        String orderByJpql = "order by ul.time desc";
         String whereJpql = conditionsWhereJpql.isEmpty() ? "" : " where " + String.join(" and ", conditionsWhereJpql);
+
         TypedQuery<UserLog> query = em.createQuery(
                 selectJpql + fromJpql + whereJpql + orderByJpql,
                 UserLog.class)
             .setFirstResult((int) pageable.getOffset())
             .setMaxResults(pageable.getPageSize());
+
         TypedQuery<Long> countingQuery= em.createQuery(
             countingJpqlSelect + fromJpql + whereJpql,
             Long.class);
