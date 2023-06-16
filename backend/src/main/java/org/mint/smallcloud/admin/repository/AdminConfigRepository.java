88ebea75acba9e420b2fd6978e8478242a29c21a
kindbear <dlgkdrb2@naver.com>
2023-06-16 15:12:36 +0900

    [BE] FIX: admin config

--- backend/src/main/java/org/mint/smallcloud/admin/repository/AdminConfigRepository.java
+++ backend/src/main/java/org/mint/smallcloud/admin/repository/AdminConfigRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminConfigRepository extends JpaRepository<AdminConfig, Long> {
-    AdminConfig findByCode(String code);
+    AdminConfig findByCodeOrOrderByLocalDateTimeDesc(String code);
 }
